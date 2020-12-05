f5eb48ff77f0c07db832351aab54eb8b34584f1e
Artem Shahoval <artem10-messi10@ukr.net>
2020-12-05 10:27:56 +0300

    h-w 25 JS-operators updated

--- homeworks_js/h.w-25/javascript.js
+++ homeworks_js/h.w-25/javascript.js
@@ -55,3 +55,151 @@ console.log('Совпадений не найдено');
 Високосный год либо кратен 400, либо кратен 4 и при этом не кратен 100.*/
 const leapYear = (year) => (year % 100 === 0 ? year % 400 === 0 : year % 4 === 0);
 console.log(leapYear(2020)); // true
+
+/*6. Написать конвертор валют. Пользователь вводит количество USD, выбирает, 
+в какую валюту хочет перевести EUR, UAN или AZN, и получает в ответ соответствующую сумму.*/
+let USD = 30;
+const EUR = 0.84;
+const UAN = 28.50;
+const AZN = 1.70;
+let result;
+let currency = "UAN";
+switch (currency) {
+  case "EUR": result = USD * EUR;
+    break;
+  case "UAN": result = USD * UAN;
+    break;
+  case "AZN": result = USD * AZN;
+    break;
+  default: result = "not correct currency";
+}
+console.log(result);
+
+/*7. Запросить у пользователя сумму покупки и вывести сумму к оплате со скидкой: от 200 до 300 – скидка будет 3%, 
+от 300 до 500 – 5%, от 500 и выше – 7%.*/
+let sale1 = 0.03;
+let sale2 = 0.05;
+let sale3 = 0.07;
+let amount = 500;
+if (amount >= 200 && amount < 300) {
+  console.log(amount = amount - (amount * sale1))
+} else if (amount >= 300 && amount < 500) {
+  console.log(amount = amount - (amount * sale2))
+} else if (amount >= 500) {
+  console.log(amount = amount - (amount * sale3))
+} else {
+  console.log("unknown")
+}
+
+/*8. Запросить у пользователя длину окружности и периметр квадрата. 
+Определить, может ли такая окружность поместиться в указанный квадрат.*/
+let circleLength = 80;
+let squarePerimetr = 90;
+
+if (circleLength < squarePerimetr) {
+  console.log("Circle will fit in the square")
+} else if (circleLength > squarePerimetr) {
+  console.log("Circle will not fit in the square!")
+} else {
+  console.log("undefinded")
+}
+
+/*9. Задать пользователю 3 вопроса, в каждом вопросе по 3 варианта ответа. За каждый правильный ответ начисляется 2 балла. 
+После вопросов выведите пользователю количество набранных баллов.*/
+let questionToUser1 = +prompt("How much will be 2 * 2: \n6, 4, 8?");
+let questionToUser2 = +prompt("How much will be 5 * 5: \n25, 30, 35?");
+let questionToUser3 = +prompt("How much will be 8 * 7: \n50, 54, 56?");
+let rightAnswer = 0;
+if (questionToUser1 == 4) {
+  console.log(rightAnswer += 2)
+} else {
+  console.log(`Wrong Answer!`)
+}
+if (questionToUser2 == 25) {
+  console.log(rightAnswer += 2)
+} else {
+  console.log(`Wrong Answer!`)
+}
+if (questionToUser3 == 56) {
+  console.log(rightAnswer += 2)
+} else {
+  console.log(`Wrong Answer!`)
+}
+console.log(`In general you received ${rightAnswer} points`)
+
+/*10. Запросить дату (день, месяц, год) и вывести следующую за ней дату. 
+Учтите возможность перехода на следующий месяц, год, а также високосный год.*/
+let day = +prompt("Enter your day");
+let month = +prompt("Enter your month");
+let year = +prompt("Enter your year");
+let nextDay = day;
+let nextMonth = month;
+let nextYear = year;
+let date;
+
+if (day > 0 && year > 0 && day < 32) {
+    switch (month) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+                if (day == 31) {
+                    nextDay = 1;
+
+                    if (month == 12) {
+                        nextMonth == 1;
+                        nextYear == year + 1;
+                    } else {
+                        nextMonth = month + 1;
+                    }
+                } else {
+                    nextDay = day + 1
+                }
+                date = `day: ${nextDay}; month: ${nextMonth}; year: ${nextYear}`;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                if (day == 30) {
+                    nextDay = 1;
+                    nextMonth = month + 1;
+                } else {
+                    if (day > 30) {
+                        date = "The date is wrong";
+                        break;
+                    } else {
+                        nextDay = day + 1;
+                    }
+                }
+                date = `day: ${nextDay}; month: ${nextMonth}; year: ${nextYear}`;
+                break;
+            case 2:
+                if (day == 29 && year % 4 == 0 ) { 
+                    nextDay = 1;
+                    nextMonth = month + 1 
+                } else if (day == 28 && year % 4 != 0) {
+                    nextDay = 1;
+                    nextMonth = month + 1 
+                } else {
+                    if ((year % 4 == 0 && day > 29) || (year % 4 != 0 && day > 28)) {
+                        date = "The date is wrong";
+                        break;
+                    } else {
+                        nextDay = day + 1;
+                    }
+                }
+                date = `day: ${nextDay}; month: ${nextMonth}; year: ${nextYear}`;
+                break;
+            default:
+                date = "The date is wrong";
+                break;
+        }
+    } else {
+        date = "The date is wrong";   
+    }
+
+    console.log(date);
