15b03369f47ace16b73521fba167c60fa28926fd
Artem Shahoval <artem10-messi10@ukr.net>
2020-12-21 16:07:41 +0200

    h-w JS-arrays still in work

--- homeworks_js/h.w-28/javascript.js
+++ homeworks_js/h.w-28/javascript.js
@@ -133,4 +133,71 @@ let resToZero = reduceArr.reduce(function(total, num) {
      return false;
    }
 });
-console.log(resToZero)
+console.log(resToZero);
+
+
+// Функция принимает объект-прямоугольник и возвращает его ширину.(без "this")
+let rectangle = {
+  top: {
+    x: 1, 
+    y: 10
+  }, 
+  bottom: {
+    x: 5, 
+    y: 3
+  },
+  getWidth: function() {
+    return (rectangle.bottom.x - rectangle.top.x);
+  }
+}
+
+let width = rectangle.getWidth();
+console.log(width);
+
+
+
+
+//1. Создать массив «Список покупок». Каждый элемент массива является объектом, который содержит название продукта, 
+//необходимое количество и куплен или нет. Написать несколько функций для работы с таким массивом.
+class Product {
+  constructor(params = {}) {
+    Object.assign(this, {
+      pName: null, 
+      amount: 1, 
+      bought: false
+    }, { ...params }); 
+  }
+
+  static sortDefault(a, b) {  // чтобы сначала шли некупленные продукты, а потом – купленные
+    return (a.bought - b.bought) * 10 + a.pName.localeCompare(b.pName); 
+  }; 
+}
+Product.prototype.toString = function () {
+  return `${this.pName} (x${this.amount}), ${this.bought ? 'куплено' : 'не куплено'}`;
+}; 
+
+
+const shopList = [
+  { pName: 'Банан', amount: 2 },
+  { pName: 'Апельсин', amount: 5, bought: true },
+  { pName: 'Молоко', bought: true },
+  { pName: 'Груша', amount: 10 }
+].map(prodDef => new Product(prodDef));
+
+const addToShopList = prodDef => {  // при добавлении покупки с уже существующим в списке продуктом, необходимо увеличивать количество в существующей покупке, а не добавлять новую
+  const prod = shopList.find(prod => prod.pName === prodDef.pName); 
+  if (!prod) return shopList.push(new Product(prodDef)); 
+  prod.amount += prodDef.amount; 
+}; 
+const printShopList = () => shopList.forEach(
+  (prod, i) => console.log(`${i + 1}. ${prod}`)
+); 
+
+shopList.sort(Product.sortDefault);
+printShopList();
+console.log('---'); 
+
+addToShopList({ pName: 'Банан', amount: 3 });
+addToShopList({ pName: 'Шоколад', amount: 2 });
+shopList.sort(Product.sortDefault);
+printShopList();
