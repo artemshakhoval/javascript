4260ca32bf365136ecb71f179ad9e41ebb393fc6
Artem Shahoval <artem10-messi10@ukr.net>
2021-01-19 16:18:57 +0200

    EXAM_2 still in progress

---
+++ homeworks_js/h.w-34/dist/main.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.slick_slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+});
